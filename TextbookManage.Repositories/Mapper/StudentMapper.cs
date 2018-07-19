f5e7db6642f12aa8a4b9d463a8635ab30c7239cc
hylimeng <meng0004@gmail.com>
2018-07-19 20:32:17 +0800

    学生映射

--- TextbookManage.Repositories/Mapper/StudentMapper.cs
+++ TextbookManage.Repositories/Mapper/StudentMapper.cs
@@ -9,17 +9,19 @@ namespace TextbookManage.Repositories.Mapper
         public StudentMapper()
         {
             // Primary Key
-            this.Map(t => t.Id).Key(KeyType.Guid).Column("StudentId");
+            
             
             // Table & Column Mappings
             this.Table("Student");
+            this.Map(t => t.Id).Key(KeyType.Assigned).Column("StudentId");
             //this.Map(t => t.Id).Column("StudentId");
             this.Map(t => t.Num).Column("StudentNum");
             this.Map(t => t.Name).Column("StudentName");
             this.Map(t => t.Gender).Column("Sex");
             this.Map(t => t.Class_Id).Column("ClassID");
+            this.Map(t => t.ProfessionalClass).Ignore();
             //this.Map(t => t.EntranceDate).Column("EntranceDate");
-            //AutoMap();
+             AutoMap();
         }
     }
 }
